1f007bcdb87f8c93f75c2d47dc436d56df496276
Daniel Braaksma <danebraaksma@hotmail.com>
2022-03-23 16:34:57 -0400

    added files

---
+++ index.js
@@ -0,0 +1,9 @@
+//Materialize JS//
+
+M.AutoInit();
+        
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
