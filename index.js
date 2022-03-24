516d5e37f5f7b601b2ac7e3d0a0b73fcad827690
Daniel Braaksma <danebraaksma@hotmail.com>
2022-03-24 09:35:39 -0400

    fixed checkbox display funciton in add exercise section

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 //Materialize JS//
 
 M.AutoInit();
-        
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+
+
+//display time input funciton//
+
+const checkbox = document.getElementById('is-timed-checkbox');
+const timeInput = document.getElementById("time-input")
+
+checkbox.addEventListener('click', () => {
+    if (checkbox.checked === false){
+        timeInput.style.display = "none";
+      } else {
+        timeInput.style.display = "block";
+      }
+});
