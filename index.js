899526c8c6dad8d375175551a0e9d8f51b36fd85
Daniel Braaksma <danebraaksma@hotmail.com>
2022-03-25 18:28:45 -0400

    features added/figured out : delete exercise, delete all, reset all

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 M.AutoInit();
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
-  });
+});
 
 
 //display time input funciton//
@@ -15,20 +15,20 @@ const checkbox = document.getElementById('is-timed-checkbox');
 const timeInput = document.getElementById("time-input")
 
 checkbox.addEventListener('click', () => {
-    if (checkbox.checked === false){
+    if (checkbox.checked === false) {
         timeInput.style.display = "none";
-      } else {
+    } else {
         timeInput.style.display = "block";
-      }
+    }
 });
 
 /*************PAGE JS *******/
 
-
-//array that will store our exercises - starts off with default streching//
-// remove the delete button on the first instance of exercise w/ css
-
 const exerciseList = document.getElementById("exercise-list")
+const deleteAll = document.getElementById("delete-btn")
+const resetAll = document.getElementById("reset-btn")
+
+//IF NO EXERCISES FROM LOCAL STORAGE START OFF WITH 2 SAMPLE ONES//
 let exercises = [
     {title: "Always start with stretching!",
     description: "your choice of",
@@ -38,20 +38,20 @@ let exercises = [
 
 
 //*********render exercises to the screen***********//
-const render = () =>{
+const render = () => {
     let listHtml = ""
-    exercises.forEach((exercise)=>{
+    exercises.forEach((exercise) => {
         listHtml += `
         <li>
         <div class="collapsible-header">
             <p>${exercise.title}</p>
             <p>Time : <span id="exercise-time-rendered">${exercise.time}</span>minutes</p>
             <div>
-                <a class="waves-effect waves-light btn" id="reset-timer">Start/Stop timer</a>
-                <a class="waves-effect waves-light btn" id="delete-item">Delete</a>
+                <a class="waves-effect waves-light btn reset-timer">Start/Stop timer</a>
+                <a class="waves-effect waves-light btn delete-item">Delete</a>
             </div>
             <label>
-                <input type="checkbox" id="complete" />
+                <input type="checkbox" class="complete" />
                 <span>Done</span>
             </label>
 
@@ -59,15 +59,27 @@ const render = () =>{
         <div class="collapsible-body"><span>${exercise.description}</span></div>
     </li>
         `
-    })
 
+    })
     exerciseList.innerHTML = listHtml;
+    addDeleteFunction()
+
 }
 
-render()
 
-// console.log(exercises.findIndex(()=>exercises.time === 10));
+deleteAll.addEventListener("click", ()=>{
+    exercises = []
+    render()
+})
 
+resetAll.addEventListener("click", ()=>{
+    let checkboxes = document.querySelectorAll(".complete")
+    checkboxes.forEach(box=>{
+
+        console.log(box)
+        box.checked = false;
+    })
+})
 //******** Add a new Exercise *********/
 /* instantiate a new obj of exerecise class
 push it to exercises array, and then re-render*/
@@ -77,11 +89,24 @@ let description = document.getElementById("exercise-description")
 let time = document.getElementById("time-input")
 let form = document.getElementById("exercise-form")
 
+
+
+class Exercise {
+    constructor(title, description, time) {
+        this.title = title;
+        this.description = description;
+        this.time = time;
+    }
+
+    //add a start/stop timer button here
+
+}
+
 // listen for the form submit
 
-form.addEventListener("submit", (e)=>{
+form.addEventListener("submit", (e) => {
     e.preventDefault()
-    let newObj = new Exercise (title.value, description.value, time.value)
+    let newObj = new Exercise(title.value, description.value, time.value)
     exercises.push(newObj)
     console.log(exercises)
     title.value = ""
@@ -92,21 +117,49 @@ form.addEventListener("submit", (e)=>{
 
 
 
-class Exercise {
-    constructor (title, description, time) {
-        this.title = title;
-        this.description = description;
-        this.time = time;
-    }
 
-    //add a start/stop timer button here
-
-}
 
 /***********Delete an exercise  ************/
 // get all the delete btns
-    // add an event listener
-    // find the btn clicked, use the parentconainer stuff and find the title value,
-    //pass to the remove function
-    // iterate through exercises array and check if === the value
-        // then spice it from the array and re-render
+// add an event listener
+// find the btn clicked, use the parentconainer stuff and find the title value,
+//pass to the remove function
+// iterate through exercises array and check if === the value
+// then spice it from the array and re-render
+
+function addDeleteFunction() {
+    let deleteBtns = document.querySelectorAll(".delete-item")
+
+    deleteBtns.forEach((btn) => {
+        // console.log(exercises)
+        btn.addEventListener("click", (e) => {
+            let deleteTitle = e.target.parentElement.parentElement.firstElementChild.textContent
+            exercises.forEach((exercise, i) => {
+                if (exercise.title === deleteTitle){
+                // console.log("in second loop")
+                // console.log(exercises)
+                    exercises.splice(i, 1)
+                    render()
+                }
+            })
+        })
+    })
+}
+
+
+
+
+// let deleteTitle = e.target.parentElement.parentElement.firstElementChild.textContent
+// console.log("in first loop")
+// // console.log(deleteTitle)
+// exercises.forEach((exercise)=>{
+//     // if (exercise.title === deleteTitle){
+//     console.log("in second loop")
+//     //     exercises.splice(i, 1)
+//     // }
+
+
+// })
+
+
+render()
