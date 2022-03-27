7abb0a771f1144ce93c462af621cf0b4c7177233
Daniel Braaksma <danebraaksma@hotmail.com>
2022-03-27 10:29:55 -0400

    search results rendered to page

--- api.js
+++ api.js
@@ -11,6 +11,7 @@ export {test}
 
 let searchResults;
 const form = document.getElementById('search-form')
+const exerciseSearchContainer = document.getElementById("exercise-search-container")
 
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
@@ -30,18 +31,26 @@ form.addEventListener('submit', async function (e) {
 
 
 //decide here what to do if the exercise !have image
-// maybe add link
+//allow user to click checkbox to search only images
+// maybe add link to google search
 
 function renderSearchResults () {
     let html = ""
-    let filteredWithImage = searchResults.filter(exercise =>{
-        return exercise.data.image
-    })
-    filteredWithImage.forEach((exercise, i) =>{
+    // let filteredWithImage = searchResults.filter(exercise =>{
+    //     return exercise.data.image
+    // })
+    // console.log(filteredWithImage)
+    searchResults.forEach((exercise, i) =>{
+
         html += `
-
-
+            <div class="exercise-search-item">
+                <h5>${exercise.value}</h5>
+                <p>${exercise.data.image ? `<img src="https://wger.de${exercise.data.image}">` :
+                `no image, click <a href='https://www.google.com/search?q=${exercise.value}'>here</a> to search`}</p>
+            </div>
         `
-
     })
+    exerciseSearchContainer.innerHTML = html;
 }
+
+// const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
